45777c4440a77145d0374aba6157d5b8ab102d15
Rajputmayuri <117455385+Rajputmayuri@users.noreply.github.com>
2025-08-26 17:15:35 +0530

    Delete Python Course/loop/Nested_loops/71.py

--- Python Course/loop/Nested_loops/71.py
+++
@@ -1,8 +0,0 @@
-"""Q71. Ask N from user. N means number of lines.
-Then print the follwing patter."""
-
-n = int(input("Enter number="))
-for i in range(1, n+1):
-    for j in range(1, 6):
-        print(i, end=" ")
-    print()
