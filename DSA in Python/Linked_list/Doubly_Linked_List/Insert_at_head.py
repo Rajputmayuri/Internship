47a794b496ecf6258591863d1236aaeee6279191
Rajputmayuri <117455385+Rajputmayuri@users.noreply.github.com>
2025-11-03 13:14:55 +0530

    Add files via upload

---
+++ DSA in Python/Linked_list/Doubly_Linked_List/Insert_at_head.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_head(self, val):
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+
+dll = DoublyLinkedList()
+dll.insert_at_head(10)
+dll.insert_at_head(20)
