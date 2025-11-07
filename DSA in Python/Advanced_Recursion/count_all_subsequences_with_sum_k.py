fd0c0dac18747cf81297d77cde8c89128dc983d5
Rajputmayuri <117455385+Rajputmayuri@users.noreply.github.com>
2025-11-07 13:17:41 +0530

    Update count_all_subsequences_with_sum_k.py

--- DSA in Python/Advanced_Recursion/count_all_subsequences_with_sum_k.py
+++ DSA in Python/Advanced_Recursion/count_all_subsequences_with_sum_k.py
@@ -1,17 +1,23 @@
-class Solution:
-    def backtrack(self, subset, index, total):
-        if total == K:
-            return True
-        if index >= N:
-            return False
+from typing import List
 
-        subset.append(arr[index])
-        pick = self.backtrack(subset, index + 1, total + arr[index])
-        if pick == True:
-            return True
 
-        e = subset.pop()
-        return self.backtrack(subset, index + 1, total)
+def backtrack(index: int, total: int) -> int:
+    # base cases
+    if total == k:
+        return 1
+    if index >= len(nums) or total > k:
+        return 0
 
-    def checkSubsequenceSum(self, N, arr, K):
-        return self.backtrack([], 0, 0)
+    # include nums[index]
+    pick = backtrack(index + 1, total + nums[index])
+    # exclude nums[index]
+    not_pick = backtrack(index + 1, total)
+
+    return pick + not_pick
+
+
+nums = [1, 2, 3, 4, 3, 2, 1, 1, 1, 1]
+k = 3
+
+count = backtrack(0, 0)
+print(count)
