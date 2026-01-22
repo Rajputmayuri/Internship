b3d7a67db30dec3fd4764104b993600c279851c1
Rajputmayuri <117455385+Rajputmayuri@users.noreply.github.com>
2026-01-22 12:33:04 +0530

    Add files via upload

---
+++ JAVA/Introduction_to_Java/literals.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class literals {
+    static void main(String[] args) {
+        byte age=45;
+        int age2=54;
+        short age3=88;
+        long ageDino=56666666666666L;
+        char ch='A';
+        float f1=5.6f;
+        boolean a=true;
+        System.out.println(age);
+        String str="Mayuri";
+        System.out.println(str);
+
+    }
+}
