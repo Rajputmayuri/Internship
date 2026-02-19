f00608f86f9277ab36a66e621055d54d673c0997
Rajputmayuri <117455385+Rajputmayuri@users.noreply.github.com>
2026-02-19 17:36:16 +0530

    Update 38.py

--- Python Course/conditional_statements/Nested if-else question/38.py
+++ Python Course/conditional_statements/Nested if-else question/38.py
@@ -2,15 +2,21 @@
 the largest one using nested if-else statements. Make sure all 3 numbers
 entered by user are dierent."""
 
-n1 = int(input("Enter 1st number="))
-n2 = int(input("Enter 2nd number="))
-n3 = int(input("Enter 3rd number="))
-if n1 == n2 or n1 == n3 or n2 == n3:
-    if n1 > n2 and n1 > n3:
-        print(n1, "is large")
-    elif n2 > n1 and n2 > n3:
-        print(n2, "is large")
-    else:
-        print(n3, "is large")
+n1 = float(input("Enter first number ="))
+n2= float(input("Enter second number ="))
+n3= float (input("Enter third number="))
+
+if a==b or b==c or a==c:
+    print("Error : All three numbers must be different.")
 else:
-    print("Enter different number")
+    if a>b:
+        if a>c:
+            largest=a
+        else:
+            largest=c
+    else:
+        if b>c:
+            largest=b
+        else:
+            largest=c
+    print(f"The largest number is :{largest}")
